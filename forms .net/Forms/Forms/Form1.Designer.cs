4e4cf605fb400ca90de47e14679587bd0aa477d4
David Espinoza <165340445+DavidEspinoza677@users.noreply.github.com>
2024-10-04 08:49:20 -0600

    forms .net
    forms .net

---
+++ forms .net/Forms/Forms/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace Forms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtEdad = new System.Windows.Forms.TextBox();
+            this.tbGrade = new System.Windows.Forms.TextBox();
+            this.tbGender = new System.Windows.Forms.TextBox();
+            this.lstDatos = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(43, 184);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(151, 68);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Mostrar";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nombre:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(40, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Edad:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(40, 113);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Nota:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(40, 138);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Genero";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(94, 59);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 20);
+            this.txtNombre.TabIndex = 5;
+            this.txtNombre.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtEdad
+            // 
+            this.txtEdad.Location = new System.Drawing.Point(94, 87);
+            this.txtEdad.Name = "txtEdad";
+            this.txtEdad.Size = new System.Drawing.Size(100, 20);
+            this.txtEdad.TabIndex = 6;
+            // 
+            // tbGrade
+            // 
+            this.tbGrade.Location = new System.Drawing.Point(94, 114);
+            this.tbGrade.Name = "tbGrade";
+            this.tbGrade.Size = new System.Drawing.Size(100, 20);
+            this.tbGrade.TabIndex = 7;
+            // 
+            // tbGender
+            // 
+            this.tbGender.Location = new System.Drawing.Point(94, 141);
+            this.tbGender.Name = "tbGender";
+            this.tbGender.Size = new System.Drawing.Size(100, 20);
+            this.tbGender.TabIndex = 8;
+            // 
+            // lstDatos
+            // 
+            this.lstDatos.FormattingEnabled = true;
+            this.lstDatos.Location = new System.Drawing.Point(301, 59);
+            this.lstDatos.Name = "lstDatos";
+            this.lstDatos.Size = new System.Drawing.Size(129, 121);
+            this.lstDatos.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(477, 401);
+            this.Controls.Add(this.lstDatos);
+            this.Controls.Add(this.tbGender);
+            this.Controls.Add(this.tbGrade);
+            this.Controls.Add(this.txtEdad);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtEdad;
+        private System.Windows.Forms.TextBox tbGrade;
+        private System.Windows.Forms.TextBox tbGender;
+        private System.Windows.Forms.ListBox lstDatos;
+    }
+}
+
---
+++ forms .net/Forms/Forms/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Forms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+
+        // Evento del botón para mostrar y guardar los datos
+        private void btnMostrarGuardar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Capturar los datos de los TextBox
+                string nombre = txtNombre.Text;
+                int edad = int.Parse(txtEdad.Text);
+                int nota = int.Parse(txtNota.Text);
+                char genero = char.Parse(txtGenero.Text);
+
+                // Mostrar los datos en el ListBox
+                lstDatos.Items.Add($"Nombre: {nombre}, Edad: {edad}, Nota: {nota}, Género: {genero}");
+
+                // Guardar los datos en un archivo binario
+                using (FileStream archivo = new FileStream("datos.dat", FileMode.Append, FileAccess.Write))
+                using (BinaryWriter escritor = new BinaryWriter(archivo))
+                {
+                    escritor.Write(nombre.Length); // Escribir la longitud del nombre
+                    escritor.Write(nombre.ToCharArray()); // Escribir el nombre
+                    escritor.Write(edad); // Escribir la edad
+                    escritor.Write(nota); // Escribir la nota
+                    escritor.Write(genero); // Escribir el género
+                }
+
+                // Limpiar los TextBox después de agregar y guardar los datos
+                txtNombre.Clear();
+                txtEdad.Clear();
+                txtNota.Clear();
+                txtGenero.Clear();
+
+                MessageBox.Show("Datos guardados correctamente.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        // Evento del botón para cargar y mostrar los datos desde el archivo binario
+        private void btnCargarDatos_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                lstDatos.Items.Clear(); // Limpiar el ListBox antes de cargar nuevos datos
+
+                // Leer los datos desde el archivo binario
+                using (FileStream archivo = new FileStream("datos.dat", FileMode.Open, FileAccess.Read))
+                using (BinaryReader lector = new BinaryReader(archivo))
+                {
+                    while (archivo.Position != archivo.Length)
+                    {
+                        int longitudNombre = lector.ReadInt32(); // Leer la longitud del nombre
+                        char[] nombreArray = lector.ReadChars(longitudNombre); // Leer el nombre
+                        string nombre = new string(nombreArray); // Convertir a string
+                        int edad = lector.ReadInt32(); // Leer la edad
+                        int nota = lector.ReadInt32(); // Leer la nota
+                        char genero = lector.ReadChar(); // Leer el género
+
+                        // Mostrar los datos en el ListBox
+                        lstDatos.Items.Add($"Nombre: {nombre}, Edad: {edad}, Nota: {nota}, Género: {genero}");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+    }
+}
